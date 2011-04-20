25e435c6c38d5c44c1226656ef086d7a0dc8ecf6
Georges Discry <georges.discry@epfl.ch>
2011-04-20 18:18:59 +0200

    split code in multiple sub-projects

--- project/build/MenthorProject.scala
+++ project/build/MenthorProject.scala
@@ -1,6 +1,22 @@
 import sbt._
 
-class MenthorProject(info: ProjectInfo) extends DefaultProject(info) with
-    AkkaProject {
+class MenthorProject(info: ProjectInfo) extends ParentProject(info) {
+
+  lazy val libmenthor = project("libmenthor", "Menthor Library")
+
+  lazy val libmenthor_akka = project("libmenthor-akka", "Menthor Library with"
+    + " Akka", new DefaultProject(_) with AkkaProject)
+
+  lazy val examples = project("examples", "Menthor Examples", new Examples(_))
+
+  class Examples(info: ProjectInfo) extends ParentProject(info) {
+    lazy val pagerank = project("pagerank", "Wikipedia Pagerank", libmenthor)
+
+    lazy val pagerank_akka = project("pagerank-akka", "Wikipedia Pagerank with"
+      + " Akka", libmenthor_akka)
+
+    lazy val sssp = project("sssp", "Single Source Shortest Paths", libmenthor)
+  }
+
   val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
 }
