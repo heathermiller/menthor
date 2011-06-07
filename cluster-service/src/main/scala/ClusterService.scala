da09218e1fc46b70e7906bdd82b544aa11cc08a6
Georges Discry <murmex@gathex.net>
2011-06-07 19:31:20 +0200

    don't try to shutdown gracefully

--- cluster-service/src/main/scala/ClusterService.scala
+++ cluster-service/src/main/scala/ClusterService.scala
@@ -6,26 +6,21 @@ import akka.serialization.RemoteActorSerialization._
 
 class ClusterService extends Actor {
   def receive = {
+    case AvailableProcessors =>
+      self.channel ! AvailableProcessors(Runtime.getRuntime.availableProcessors)
     case CreateForeman(parent) =>
       val foreman = actorOf(new Foreman(parent)).start()
       self.channel ! ForemanCreated(foreman)
-    case x => println(x)
-  }
-
-  override def postStop() {
-    registry.shutdownAll()
-    remote.shutdown()
   }
 }
 
 object ClusterService {
   def run() {
-    remote.start()
     remote.register(actorOf[ClusterService])
+    remote.start()
   }
 
   def main(args: Array[String]) {
     run()
   }
 }
-
