b19a1e268b85ceb82cf5aa63c5e8b41eeab1d85c
Georges Discry <murmex@gathex.net>
2011-06-10 13:04:25 +0200

    implement the foreman message routing

--- libmenthor-akka/src/main/scala/menthor/cluster/Foreman.scala
+++ libmenthor-akka/src/main/scala/menthor/cluster/Foreman.scala
@@ -1,6 +1,9 @@
 package menthor.cluster
 
-import menthor.processing.{Worker, SetupDone}
+import menthor.processing.Worker
+import menthor.processing.{ Stop, Next, WorkerStatusMessage, StopRequest, Done, Halt, Crunch, CrunchResult, SetupDone }
+import Crunch.reduceCrunch
+import WorkerStatusMessage.reduceStatusMessage
 
 import akka.actor.{Actor, ActorRef}
 import collection.mutable.ListBuffer
@@ -20,13 +23,58 @@ class Foreman[Data: Manifest](val parent: ActorRef) extends Actor {
       if (self.sender.isDefined) {
         val worker = self.sender.get
         if ((remaining - worker) isEmpty) {
-          become(processing(children))
+          become(processing(children, children, Halt))
           parent ! SetupDone
         } else become(setup(children, remaining - worker))
       }
   }
 
-  def processing(children: Set[ActorRef]): Actor.Receive = {
-    case _ =>
+  def processing(children: Set[ActorRef], remaining: Set[ActorRef], status: WorkerStatusMessage): Actor.Receive = {
+    case Stop =>
+      assert(remaining.isEmpty)
+      for (child <- children)
+        child ! Stop
+      self.stop()
+    case msg @ (Next | _ : CrunchResult[_]) =>
+      assert(remaining.isEmpty)
+      for (child <- children)
+        child ! msg
+      become(processing(children, children, Halt))
+    case msg: WorkerStatusMessage =>
+      assert(self.sender.isDefined && remaining.nonEmpty)
+      val _status = reduceStatusMessage(msg, status)
+      val worker = self.sender.get
+      assert(remaining contains worker)
+
+      if ((remaining - worker) isEmpty)
+        parent ! _status
+      become(processing(children, remaining - worker, _status))
+    case _crunch: Crunch[_] =>
+      assert(self.sender.isDefined && remaining.nonEmpty)
+      assert(children.size == remaining.size)
+      val worker = self.sender.get
+      assert(remaining contains worker)
+      val crunch = _crunch.asInstanceOf[Crunch[Data]]
+
+      if ((remaining - worker) isEmpty) {
+        parent ! crunch
+        become(processing(children, remaining - worker, Halt))
+      } else
+        become(crunching(children, remaining - worker, crunch))
+  }
+
+  def crunching(children: Set[ActorRef], remaining: Set[ActorRef], crunch1: Crunch[Data]): Actor.Receive = {
+    case _crunch2: Crunch[_] =>
+      assert(self.sender.isDefined && remaining.nonEmpty)
+      val worker = self.sender.get
+      assert(remaining contains worker)
+      val crunch2 = _crunch2.asInstanceOf[Crunch[Data]]
+      val crunch = reduceCrunch(crunch1, crunch2)
+
+      if ((remaining - worker) isEmpty) {
+        parent ! crunch
+        become(processing(children, remaining - worker, Halt))
+      } else
+        become(crunching(children, remaining - worker, crunch))
   }
 }
--- libmenthor-akka/src/main/scala/menthor/processing/Messages.scala
+++ libmenthor-akka/src/main/scala/menthor/processing/Messages.scala
@@ -4,6 +4,43 @@ import menthor.datainput.DataInput
 
 import akka.actor.Uuid
 
+sealed abstract class InternalMessage
+
+sealed abstract class ControlMessage extends InternalMessage
+
+case object Stop extends ControlMessage
+
+case object Next extends ControlMessage
+
+object WorkerStatusMessage {
+  def reduceStatusMessage(ctrl1: WorkerStatusMessage, ctrl2: WorkerStatusMessage) = (ctrl1, ctrl2) match {
+    case (StopRequest, _) | (_, StopRequest) => StopRequest
+    case (Done, _) | (_, Done) => Done
+    case (Halt, Halt) => Halt
+  }
+}
+
+sealed abstract class WorkerStatusMessage extends InternalMessage
+
+case object StopRequest extends WorkerStatusMessage
+
+case object Done extends WorkerStatusMessage
+
+case object Halt extends WorkerStatusMessage
+
+sealed abstract class CrunchMessage extends InternalMessage
+
+object Crunch {
+  def reduceCrunch[Data](crunch1: Crunch[Data], crunch2: Crunch[Data]) = {
+    assert(crunch1.cruncher == crunch2.cruncher)
+    Crunch(crunch1.cruncher, crunch1.cruncher(crunch1.result, crunch2.result))
+  }
+}
+
+case class Crunch[Data](cruncher: (Data, Data) => Data, result: Data) extends CrunchMessage
+
+case class CrunchResult[Data](result: Data) extends CrunchMessage
+
 case class Message[Data](dest: VertexRef, value: Data)(
   implicit val source: VertexRef,
   implicit val step: Int
@@ -35,8 +72,8 @@ case object VerticesShared extends SetupMessage
 
 case class RequestVertexRef[VertexID](vid: VertexID)(
   implicit val manifest: Manifest[VertexID]
-)
+) extends SetupMessage
 
 case class VertexRefForID[VertexID](vid: VertexID, vertexUuid: Uuid, workerUuid: Uuid)(
   implicit val manifest: Manifest[VertexID]
-)
+) extends SetupMessage
