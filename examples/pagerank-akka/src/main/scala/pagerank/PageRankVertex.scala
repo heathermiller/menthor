e8934246c00ed212ccb5770a50b08a7ba83bb0ba
Georges Discry <murmex@gathex.net>
2011-06-24 03:03:34 +0200

    use the same behavior as the parallel pagerank

--- examples/pagerank-akka/src/main/scala/pagerank/PageRankVertex.scala
+++ examples/pagerank-akka/src/main/scala/pagerank/PageRankVertex.scala
@@ -13,7 +13,7 @@ class PageRankVertex(val label: String, _margin: Double = 0.00001d, val damping:
     val diff = (newvalue - value).abs
     value = newvalue
 
-    if ((diff > margin) && (superstep != iterations)) {
+    if ((diff > margin) && (step != iterations)) {
       val n = neighbors.size
       for (neighbor <- neighbors)
         yield Message(neighbor, value / n)
@@ -21,100 +21,4 @@ class PageRankVertex(val label: String, _margin: Double = 0.00001d, val damping:
       voteToHalt
     }
   }
-
-  override def initialize {
-    value = 1.0d / numVertices
-  }
 }
-/*
-object PageRank {
-
-  def runPageRank(iterations: Int) {
-    println("running PageRank...")
-    println("Creating new graph...")
-    var g: Graph[Double] = null
-    val ga = actorOf({ g = new Graph[Double]; g })
-    
-    // a little web graph: BBC -> MS, EPFL -> BBC, PHILIPP -> BBC, PHILIPP -> EPFL
-    val d1 = g.addVertex(new PageRankVertex("BBC"))
-    val d2 = g.addVertex(new PageRankVertex("MS"))
-    val d3 = g.addVertex(new PageRankVertex("EPFL"))
-    val d4 = g.addVertex(new PageRankVertex("PHILIPP"))
-    d1.connectTo(d2)
-    d3.connectTo(d1)
-    d4.connectTo(d1)
-    d4.connectTo(d3)
-
-    g.synchronized {
-      for (v <- g.vertices) {
-        v.value = 0.25d
-      }
-    }
-
-    println("values before propagation:")
-    g.synchronized {
-      for (v <- g.vertices) {
-        println(v.label + ": " + v.value)
-      }
-    }
-
-    ga.start()
-    g.iterate(iterations)
-
-    println("values after propagation:")
-    g.synchronized {
-      for (v <- g.vertices) {
-        println(v.label + ": " + v.value)
-      }
-    }
-
-    g.terminate()
-  }
-
-  import java.io.{FileWriter, PrintWriter}
-
-  def runWikipediaRank(numIterations: Int, dataDir: String, numPages: Int, small: Boolean) {
-    println("Reading wikipedia graph from file...")
-    //val lines = scala.io.Source.fromFile("testgraph.txt").getLines()
-
-    val linesOrig = scala.io.Source.fromFile(dataDir + (if (small) "links-sorted-small.txt" else "links-simple-sorted.txt")).getLines()
-    val lines = if (small)
-      linesOrig.take(numPages)
-    else {
-      // drop first 150'000 entries
-      linesOrig.drop(150000).take(numPages)
-    }
-    val (wikigraph, ga) = GraphReader.readGraph(lines)
-    //GraphReader.printGraph(wikigraph)
-    println("#vertices: " + wikigraph.vertices.size)
-
-    println("Building page title map...")
-    val names: Map[String, String] = new HashMap[String, String] {
-      override def default(label: String) = {
-        "no_title[" + label + "]"
-      }
-    }
-    
-    val titles = scala.io.Source.fromFile(dataDir + "titles-sorted-small.txt").getLines()
-    for ((title, i) <- titles.take(400000) zipWithIndex)
-      names.put("" + i, title)
-
-    ga.start()
-    wikigraph.iterate(numIterations)
-
-    wikigraph.synchronized {
-      val sorted = wikigraph.vertices.sortWith((v1: Vertex[Double], v2: Vertex[Double]) => v1.value > v2.value)
-      for (page <- sorted.take(10)) {
-        println(names(page.label) + " has rank " + page.value)
-      }
-    }
-
-    wikigraph.terminate()
-  }
-
-  def main(args: Array[String]) {
-    runWikipediaRank(args(0).toInt, args(1), args(2).toInt, true) // numPages = 20'000
-    //runPageRank(args(0).toInt)
-  }
-
-}*/
